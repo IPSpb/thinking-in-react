901d166add2fbd26703f43b7e4d640a57a43b96a
ivpopov <iv.popov@estp.ru>
2016-03-25 10:46:24 +0300

    First commit

---
+++ webpack.config.js
@@ -0,0 +1,59 @@
+'use strict';
+
+const webpack = require('webpack');
+
+module.exports = {
+    context: __dirname + '/app',
+    // Точки входа
+    entry: './app',
+
+    watch: true,
+    // Сколько ждать при изменении файла чтоб watch отработал
+    watchOptions: {
+        aggregateTimeout: 100
+    },
+
+    // Куда нужно выкладывать
+    output: {
+        path:       __dirname + '/public/build',
+        publicPath: '/build/',
+        filename:   '[name].js'
+    },
+
+    // Где икать обычные файлы (более быстрая сборка)
+    resolve: {
+        modulesDirectories: ['node_modules'],
+        extensions:         ['', '.js']
+    },
+
+    // Где искать лоадеры (более быстрая сборка)
+    resolveLoader: {
+        modulesDirectories: ['node_modules'],
+        moduleTemplates:    ['*-loader', '*'],
+        extensions:         ['', '.js']
+    },
+
+    // Модули
+    module: {
+        loaders: [
+            {
+                test:   /\.js$/,
+                loader: 'babel',
+                exclude: /(node_modules)/,
+                query: {
+                    presets: ['react', 'es2015'],
+                    plugins: ['transform-runtime']
+                }
+            }
+        ]
+    },
+
+    plugins: [
+        new webpack.NoErrorsPlugin(),
+        new webpack.ProvidePlugin({
+            $:               'jquery',
+            jQuery:          'jquery',
+            'window.jQuery': 'jquery'
+        })
+    ]
+};
